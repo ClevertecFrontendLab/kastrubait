be443aa4f1e0776a7fa63cdbb68e03d57efc89ce
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2024-02-28 06:01:45 +0300

    refactor: styles

--- src/components/footer/main-footer.tsx
+++ src/components/footer/main-footer.tsx
@@ -7,16 +7,22 @@ import s from './main-footer.module.scss';
 export const MainFooter: React.FC = () => {
 
     return (
-        <div className={s.main_footer}>
+        <footer className={s.main_footer}>
            <Button type='text' className={s.footer_review}>Смотреть отзывы</Button>
-           <Card bordered={false} className={s.footer_card}>
-                <p className={s.card_title}>Скачать на телефон</p>
-                <p className={s.card_text}>Доступно в PRO-тарифе</p>
+           <Card
+                bordered={false}
+                className={s.footer_card}
+                >
+                {/* <div> */}
+                    <p className={s.card_title}>Скачать на телефон</p>
+                    <p className={s.card_text}>Доступно в PRO-тарифе</p>
+                {/* </div> */}
+
                 <div className={s.card_fone}>
                     <Button type='text' icon={<AndroidFilled />}>Android OS</Button>
                     <Button type='text' icon={<AppleFilled />}>Apple iOS</Button>
                 </div>
             </Card>
-        </div>
+        </footer>
     );
 };
